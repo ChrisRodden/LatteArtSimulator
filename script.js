05e7ab7bbd01828df8bdb54daba8bee72dafad03
ChrisRodden <121725683+ChrisRodden@users.noreply.github.com>
2022-12-31 22:34:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1499,7 +1499,7 @@ function generateColor () {
     c.g *= 0.15;
     c.b *= 0.15;
     return c;
-    else
+    if (COLORFUL == false)
     let c = HSVtoRGB(0.0, 0.0, 100.0);
     c.r *= 0.15;
     c.g *= 0.15;
