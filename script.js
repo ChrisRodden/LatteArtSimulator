9f872678e9634bcef184debdd1f8c70a1e0c88dc
ChrisRodden <121725683+ChrisRodden@users.noreply.github.com>
2022-12-31 19:17:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1482,7 +1482,7 @@ function correctDeltaY (delta) {
 }
 
 function generateColor () {
-    let c = HSVtoRGB(Math.random(), 1.0, 1.0);
+    let c = HSVtoRGB(0.0, 0.0, 100.0);
     c.r *= 0.15;
     c.g *= 0.15;
     c.b *= 0.15;
