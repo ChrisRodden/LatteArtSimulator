7d242feaba825bbc45dcc527b818ba3d21d379e5
ChrisRodden <121725683+ChrisRodden@users.noreply.github.com>
2023-01-01 01:10:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -158,13 +158,13 @@ function startGUI () {
     var gui = new dat.GUI({ width: 300 });
     //gui.add(config, 'DYE_RESOLUTION', { 'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('quality').onFinishChange(initFramebuffers);
     //gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256 }).name('sim resolution').onFinishChange(initFramebuffers);
-    gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('density diffusion');
-    gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('velocity diffusion');
+    gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('Fade Speed');
+    gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('Viscosity');
     gui.add(config, 'PRESSURE', 0.0, 1.0).name('pressure');
     //gui.add(config, 'CURL', 0, 50).name('vorticity').step(1);
-    gui.add(config, 'SPLAT_RADIUS', 0.01, 1.0).name('splat radius');
+    gui.add(config, 'SPLAT_RADIUS', 0.01, 1.0).name('Pour Radius');
     //gui.add(config, 'SHADING').name('shading').onFinishChange(updateKeywords);
-    gui.add(config, 'COLORFUL').name('colorful');
+    gui.add(config, 'COLORFUL').name('Colorful');
     //gui.add(config, 'PAUSED').name('paused').listen();
 
     gui.add({ fun: () => {
@@ -176,24 +176,15 @@ function startGUI () {
     //bloomFolder.add(config, 'BLOOM_INTENSITY', 0.1, 2.0).name('intensity');
     //bloomFolder.add(config, 'BLOOM_THRESHOLD', 0.0, 1.0).name('threshold');
 
-    let sunraysFolder = gui.addFolder('Sunrays');
-    sunraysFolder.add(config, 'SUNRAYS').name('enabled').onFinishChange(updateKeywords);
-    sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('weight');
+    //let sunraysFolder = gui.addFolder('Sunrays');
+    //sunraysFolder.add(config, 'SUNRAYS').name('enabled').onFinishChange(updateKeywords);
+    //sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('weight');
 
     let captureFolder = gui.addFolder('Capture');
     captureFolder.addColor(config, 'BACK_COLOR').name('background color');
     captureFolder.add(config, 'TRANSPARENT').name('transparent');
     captureFolder.add({ fun: captureScreenshot }, 'fun').name('take screenshot');
 
-            let twitter = gui.add({ fun : () => {
-        ga('send', 'event', 'link button', 'twitter');
-        window.open('https://twitter.com/OmraCoffee');
-    } }, 'fun').name('Twitter');
-    twitter.__li.className = 'cr function bigFont';
-    twitter.__li.style.borderLeft = '3px solid #8C8C8C';
-    let twitterIcon = document.createElement('span');
-    twitter.domElement.parentElement.appendChild(twitterIcon);
-    twitterIcon.className = 'icon twitter';
     
             let instagram = gui.add({ fun : () => {
         ga('send', 'event', 'link button', 'instagram');
@@ -224,6 +215,17 @@ function startGUI () {
     let facebookIcon = document.createElement('span');
     facebook.domElement.parentElement.appendChild(facebookIcon);
     facebookIcon.className = 'icon facebook';
+    
+            let twitter = gui.add({ fun : () => {
+        ga('send', 'event', 'link button', 'twitter');
+        window.open('https://twitter.com/OmraCoffee');
+    } }, 'fun').name('Twitter');
+    twitter.__li.className = 'cr function bigFont';
+    twitter.__li.style.borderLeft = '3px solid #8C8C8C';
+    let twitterIcon = document.createElement('span');
+    twitter.domElement.parentElement.appendChild(twitterIcon);
+    twitterIcon.className = 'icon twitter';
+    
 
     if (isMobile())
         gui.close();
